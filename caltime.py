5075e2bd84a2dc2641225a90ebbaf6f12236a2e1
David Jørgensen <david@kaustik.com>
2015-03-20 14:34:40 +0100

    Version 1.0!

--- caltime.py
+++ caltime.py
@@ -3,18 +3,44 @@
 import datetime
 import re
 import time
+import os, sys
 
-validWords = ['DESCRIPTION', 'SUMMARY', 'LOCATION', 'DTSTART', 'DTEND']
+try:
+	import config
+except IOError:
+	print "No configuration :("
+	exit(1)
+
+words = ['DTSTART', 'DTEND', 'SUMMARY', 'LOCATION']
+
+def createComponent(array):
+	component = {}
+	a = array.split(':')
+
+	for word in words:
+		if a[0].startswith(word):
+			a[0] = word
+			return (a[0], a[1].strip())
+
+	return (False, False)
 
 def createEvent(array):
 
 	event = {}
 
 	for line in array:
-		l = line.split(':')
+		name, value = createComponent(line)
+		if False != value:
+			event[name] = value
 
-		if l[0] in validWords:
-			event[l[0]] = l[1]
+	start = datetime.datetime.strptime(event['DTSTART'], '%Y%m%dT%H%M%S')
+	event['DTSTART'] = start
+
+	end = datetime.datetime.strptime(event['DTEND'], '%Y%m%dT%H%M%S')
+	event['DTEND'] = end
+
+	delta = end - start
+	event['minutes'] = delta.seconds / 60
 
 	return event
 			
@@ -23,18 +49,8 @@ def calc(event):
 	print "%s \n" % event
 
 
-def main():
-
+def parseFile(f):
 	insideEvent = False
-
-	try:
-		f = open("Kåda.ics", 'r');
-	except IOError:
-		print "Missing file!"
-		exit(1)
-
-	now = datetime.datetime.now()
-
 	array = []
 
 	for line in f:
@@ -44,11 +60,25 @@ def main():
 		
 		elif line.startswith("END:VEVENT"):
 			insideEvent = False
-			event = createEvent(array)
-			print event
-			array = []
+			return createEvent(array)
 
 		if insideEvent:
 			array.append(line)
+
+
+def main():
+	totalTime = 0
+
+	files = os.listdir(config.path + '/Events')
+	for file in files:
+		if os.path.isfile(config.path + '/Events' + '/' + file) == True:
+			try:
+				f = open(config.path + '/Events' + '/' + file, 'r')
+				event = parseFile(f)
+				totalTime += event['minutes']
+			except IOError:
+				print "Missing file! Continuing..."
+
+	print "Total time for current calendar is %s hours and %s minutes" % (totalTime/60, totalTime%60)
 
 main()
